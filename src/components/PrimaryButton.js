d8e3f1f55206c4c1528497c3ea4f9f8ea88d82c4
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-10-01 23:59:13 +0530

    Basic Setup Designed

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PrimaryButton = ({children}) => {
+    return <View>
+        <Text>{children}</Text>
+  </View>
+}
+
+export default PrimaryButton
