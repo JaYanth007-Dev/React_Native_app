0c77f693833b07280538c5b9750cae8f9cacd453
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-10-02 11:11:31 +0530

    `Refactor components and styles: update PrimaryButton and StartGameScreen`

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,10 +1,48 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
-const PrimaryButton = ({children}) => {
-    return <View>
-        <Text>{children}</Text>
-  </View>
+const PrimaryButton = ({ children }) => {
+  
+  function handleonPress() {
+    console.log("Pressed")
+  }
+  return (
+    <View style={style.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [style.buttonInnerContainer,style.pressed] : style.buttonInnerContainer
+        }
+        onPress={handleonPress}
+        android_ripple={{ color: "#8fdc1ca4" }}
+      >
+        <Text style={style.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
-export default PrimaryButton
+
+
+export default PrimaryButton;
+
+const style = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow:'hidden'
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity:0.75
+  }
+});
--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -22,24 +22,23 @@ export default StartGameScreen;
 
 
 const style = StyleSheet.create({
-    inputContainer: {
-        padding: 16,
-        marginTop: 100,
-        marginHorizontal:24,
-        backgroundColor: "#72063c",
-        borderRadius: 8,
-        elevation:4
-    },
-    numberInput: {
-        height: 800,
-        width:50,
-        textAlign:'center',
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight:'bold'
-    }
-
-})
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+  },
+  numberInput: {
+    height: 70,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
